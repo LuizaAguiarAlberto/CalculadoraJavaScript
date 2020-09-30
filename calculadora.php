3c74f9442c2b1d45b1026fc405f7d4038de3d7ed
Luiza <luiza@gmail.com>
2020-09-30 16:27:08 -0300

    enviando a calculadora que soma através de JavaScript, utilizando os comandos Prompt, Alert e console.log para o github

---
+++ calculadora.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Calculadora JavaScript</title>
+        <script src="calculadoraJavaScript.js"></script>
+	</head>
+	<body>
+    </body>
+</html>
---
+++ calculadoraJavaScript.js
@@ -0,0 +1,10 @@
+var n1= prompt("Digite o primeiro número:");
+var n2= prompt("Digite o segundo número:");
+
+var inteiro1 = parseInt(n1);
+var inteiro2 = parseInt(n2);
+var soma = inteiro1 + inteiro2;
+
+console.log(soma);
+
+alert("Resultado=" + soma);
